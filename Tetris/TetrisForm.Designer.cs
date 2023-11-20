287e7c007005174635d6641b442d52af535b39c1
Basti2701 <148857004+Basti2701@users.noreply.github.com>
2023-11-20 22:40:42 +0100

    Gui

---
+++ Tetris/TetrisForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Tetris
+{
+    partial class TetrisForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.panelGame = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panelGame
+            // 
+            this.panelGame.Location = new System.Drawing.Point(226, 12);
+            this.panelGame.Name = "panelGame";
+            this.panelGame.Size = new System.Drawing.Size(330, 415);
+            this.panelGame.TabIndex = 0;
+            this.panelGame.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelGame_Paint);
+            // 
+            // TetrisForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelGame);
+            this.Name = "TetrisForm";
+            this.Text = "Tetris";
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Panel panelGame;
+    }
+}
+
---
+++ Tetris/TetrisForm.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tetris
+{
+    public partial class TetrisForm : Form
+    {
+        int cellSize = 20;
+        int gamewidth = 10;
+        int gameheight = 20;
+
+        public TetrisForm()
+        {
+            InitializeComponent();
+            DrawGamefield();
+        }
+
+        private void PanelGame_Paint(object sender, PaintEventArgs e)
+        {
+            DrawGamefield();
+        }
+
+        private void DrawGamefield()
+        {
+            Graphics g = panelGame.CreateGraphics();
+            //Draw vertical Lines
+            for (int x = 0; x <= gamewidth * cellSize; x += cellSize)
+            {
+                g.DrawLine(Pens.Black, x, 0, x, gameheight * cellSize);
+            }
+
+            //Draw horizontal Lines
+            for (int y = 0; y <= gameheight * cellSize; y += cellSize)
+            {
+                g.DrawLine(Pens.Black, 0, y, gamewidth * cellSize, y);
+            }
+
+            g.Dispose();
+        }
+        +
+    }
+}
